c06fc68436f7d4ac4153e379369e6733cef2379d
Jiawen Chu <sab1025@bu.edu>
2022-11-02 18:57:54 -0400

    "Added files for deployment on Elastic Beanstalk"

--- backend/remove_product.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, url_for, render_template, redirect, request
-import sqlalchemy
-from models import db, Products
-
-remove_product = Blueprint('add_product', __name__, template_folder='../frontend')
